323150986a99c10c297560ce19a5f8d0e5054a2a
Esther Halls <esthernhalls@gmail.com>
2023-02-16 15:24:58 -0700

    Initial Commit

---
+++ SolarThermalSystem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SolarThermalSystem
+//
+//  Created by Esther on 2/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
