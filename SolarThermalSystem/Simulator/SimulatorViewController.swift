86abaebf4cd0858941279692d650d56fb82af97d
Esther Halls <esthernhalls@gmail.com>
2023-02-17 18:52:26 -0700

    UI color and interface design adjustments

--- SolarThermalSystem/Simulator/SimulatorViewController.swift
+++ SolarThermalSystem/Simulator/SimulatorViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 class SimulatorViewController: UIViewController {
     // MARK: - Outlets
     @IBOutlet weak var collectorTypePopupButton: UIButton!
+    @IBOutlet weak var systemImageView: UIImageView!
+    @IBOutlet weak var backgroundView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Create Gradient Background Layer
+        let gradientLayer = CAGradientLayer()
+        /// Set the size of the layer to be equal to the size of the display
+        gradientLayer.frame = view.bounds
+        /// Set an array of core graphics colors (.cgColor) to create the gradient
+        gradientLayer.colors = [UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1.00).cgColor, UIColor(red: 0.80, green: 0.16, blue: 0.05, alpha: 1.00).cgColor]
+        /// Rasterize this static layer to improve performance
+        gradientLayer.shouldRasterize = true
+        /// Apply the gradient to the backgroundGradient UIView
+        backgroundView.layer.insertSublayer(gradientLayer, at: 0)
+        
+        let image = UIImage(named: "solarImage")
+        DispatchQueue.main.async {
+            self.systemImageView.image = image
+        }
+        // Initial Menus Setup
         setupCollectorType()
     }
     
